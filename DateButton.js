a58e871ff796a22cbf515a33765bdff6c6244639
zackmears <zackmears@gmail.com>
2017-05-25 20:02:03 -0400

    date project

---
+++ DateButton.js
@@ -0,0 +1,26 @@
+﻿
+function DateEntry() {
+
+    var ThatDate = document.getElementById("DateInput1");{
+    var dateString = textInput.value;
+    var dateValue = Date.parse(dateString);
+        if (!isNaN(ThatDate)) {
+            alert("Please enter a date");
+        } else {
+            showOutput(DateOutput);
+        }
+
+        return false;
+
+    }
+
+    function init() {
+        var button = document.getElementById("addButton");
+        button.onclick = DateEntry;
+
+    }
+
+
+
+    window.onload = init;
+}
